c3555ddd543a5dbc95c1f0f547c6648cac018d93
Leonardo Aguirre <leonardo@oddsactions.com.br>
2015-03-05 15:17:03 -0300

    Implementa app Institucional

--- fenavist/urls.py
+++ fenavist/urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'fenavist.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'institucional.views.home', name='home'),
+    url(r'^institucional/sobre$', 'institucional.views.sobre', name='sobre'),
+    url(r'^institucional/equipe$', 'institucional.views.equipe', name='equipe'),
+    url(r'^institucional/parcerias$', 'institucional.views.parcerias', name='parcerias'),
+    url(r'^institucional/sindicatos$', 'institucional.views.sindicatos', name='sindicatos'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- institucional/views.py
+++ institucional/views.py
@@ -5,3 +5,22 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, 'home.html')
+
+def sobre(request):
+
+    return render(request, 'sobre.html')
+
+
+def equipe(request):
+
+    return render(request, 'equipe.html')
+
+
+def parcerias(request):
+
+    return render(request, 'parcerias.html')
+
+
+def sindicatos(request):
+
+    return render(request, 'sindicatos.html')
